4f70b35bf179ae9880a01116ee4157c9e438657d
Umair Adil <umair_adil@live.com>
2026-04-30 16:31:33 +0500

    Release v2.2.2 - Backpressure, Redaction & Search Features

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_logs_example/main.dart';
+import 'package:flutter_logs_example/flutter_logs_example.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
